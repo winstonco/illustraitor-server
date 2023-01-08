938f17398462a8913123ac80141f2ee9449e9145
Winston Co <winstonco@live.com>
2023-01-08 09:39:24 -1000

    gen prompts

--- src/utils/genPrompt.ts
+++ src/utils/genPrompt.ts
@@ -1,3 +1,18 @@
+import RandomPicker from './RandomPicker';
+
 export default function genPrompt(): string {
-  return 'Test prompt';
+  const subject: string = RandomPicker.pickOne(subjects);
+  const action: string = RandomPicker.pickOne(actions);
+  const location: string = RandomPicker.pickOne(locations);
+  const other: string = RandomPicker.pickOne(others);
+  return subject.concat(action, location, other);
+  // return 'Test prompt';
 }
+
+const subjects: string[] = ['Me '];
+
+const actions: string[] = ['walking '];
+
+const locations: string[] = ['in a field '];
+
+const others: string[] = [''];
