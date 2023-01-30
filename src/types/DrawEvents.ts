7b4671bc61c881866ef15fd1a167dec9955789bc
Winston Co <winstonco@live.com>
2023-01-29 16:24:38 -1000

    draw events include socketid

--- src/types/DrawEvents.ts
+++ src/types/DrawEvents.ts
@@ -1,16 +1,18 @@
 export default interface IDraw {
-  beginDrawing: (startX: number, startY: number) => void;
-  drawTo: (toX: number, toY: number, width?: number, color?: string) => void;
-  endDrawing: () => void;
+  beginDrawing: (id: string, startX: number, startY: number) => void;
+  drawTo: (
+    id: string,
+    toX: number,
+    toY: number,
+    width?: number,
+    color?: string
+  ) => void;
+  endDrawing: (id: string) => void;
   clearCanvas: () => void;
 }
 
 export type DrawEventArgs =
   | []
-  | [startX: number, startY: number]
-  | [
-      toX: number,
-      toY: number,
-      width?: number | undefined,
-      color?: string | undefined
-    ];
+  | [id: string]
+  | [id: string, startX: number, startY: number]
+  | [id: string, toX: number, toY: number, width?: number | undefined];
